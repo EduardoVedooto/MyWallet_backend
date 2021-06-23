336333196df543e990cdfa43011571bc73777b62
Eduardo Vedooto <vedootoeduardo@gmail.com>
2021-06-23 12:16:45 -0300

    Changed the max length of name to 40

--- src/schema/users.schema.js
+++ src/schema/users.schema.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 const usersSchema = data => {
   const schema = Joi.object({
-    name: Joi.string().trim().max(30).pattern(/^[A-Za-zÀ-úç'\s]+$/).required(),
+    name: Joi.string().trim().max(40).pattern(/^[A-Za-zÀ-úç'\s]+$/).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(6),
     confirmPassword: Joi.string().valid(Joi.ref('password')).label("Passwords don't match")
